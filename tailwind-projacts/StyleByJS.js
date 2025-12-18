f1d05439c551228c4b4ef310515ab1131b8181d0
shikebazahidi999 <shikebazahedi41@gmail.com>
2025-12-18 11:28:06 -0800

    changes

--- tailwind-projacts/StyleByJS.js
+++ tailwind-projacts/StyleByJS.js
@@ -1,4 +1,6 @@
 const body = document.body;
+const lists = document.querySelectorAll("li:nth-child(2)");
+
 // div
 const div = document.createElement("div");
 div.classList.add("div")
@@ -48,16 +50,18 @@ div.append(image,h1,p1);
 
 // secound div/////////////////////////////////////////////
 const div1 = document.createElement("div");
-div.classList.add("div")
+div1.classList.add("div");
+div1.style.marginLeft= "500px"
 body.appendChild(div1);
 // button3
 const btn3 = document.createElement("button");
 btn3.innerText= "Hide Shadow";
 btn3.classList.add("btnStyle");
+btn3.style.marginLeft = "500px";
 body.appendChild(btn3);
 // image2
 const image2 = document.createElement("img");
-image2.setAttribute("src","../imgs/Screenshot_20250924_083622.jpg");
+image2.setAttribute("src","../imgs/Screenshot_20250724_234201.jpg");
 image2.setAttribute("alt","your intertent has promlem");
 image2.classList.add("imageStyle");
 
@@ -70,9 +74,28 @@ const p2 = document.createElement("p");
 p2.innerText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi omnis cumque impedit vel laboriosam enim neque dicta et at? Facilis, nobis. Vero numquam deleniti dolore aut deserunt? Molestiae, ut nostrum!";
 p2.classList.add("p1Style");
 
-// secound btn
+// buttton 4
 const btn4 = document.createElement("button");
 btn4.innerText= "Reshow Shadow";
 btn4.classList.add("btnStyle");
-btn4.classList.add("hide")
+btn4.classList.add("hide");
+btn4.style.marginLeft = "500px";
+
 body.appendChild(btn4);
+
+// btn condition
+btn3.addEventListener("click",()=>{
+    div1.classList.add("remove");
+    image2.classList.add("remove");
+    btn3.classList.add("hide");
+    btn4.classList.remove("hide")
+});
+btn4.addEventListener("click",()=>{
+    div1.classList.remove("remove");
+    image2.classList.remove("remove");
+    btn3.classList.remove("hide");
+    btn4.classList.add("hide")
+})
+div1.append(image2,h12,p2);
+
+
