4d2b13789a18970179ee35397ec76afb6af36fff
shikebazahidi999 <shikebazahedi41@gmail.com>
2025-12-30 15:22:44 +0430

    changes

--- ifStatement/selector.js
+++ ifStatement/selector.js
@@ -12,7 +12,10 @@ const div = h1.parentElement;
 div.classList.add("divSt");
 // next sibling
 const nexth1 = ul.nextElementSibling;
-nexth1.classList.add("h2")
+nexth1.classList.add("h2");
+const newTag = ul.insertAdjacentHTML("beforebegin","<h1>used insertAdjacentHTNL beforbegin</h1>");
+const secoundTag = ul.insertAdjacentHTML("afterend","<h1>used afterend for this messege</h1>");
+const thirdTag =ul.insertAdjacentHTML("beforeend","<h2>used from beforend</h2> ")
 
 
 
