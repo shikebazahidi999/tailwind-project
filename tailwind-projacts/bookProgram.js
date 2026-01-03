01cb123524f788bd25d2d4515a5f6c472918ab6e
shikebazahidi999 <shikebazahedi41@gmail.com>
2026-01-03 09:38:49 +0430

    changes

---
+++ tailwind-projacts/bookProgram.js
@@ -0,0 +1,36 @@
+// nabbar weith 85%
+const bars = document.getElementById("bars");
+const listbook = document.getElementById("listbook");
+const showform = document.getElementById("showform");
+const izafakardan = document.getElementById("izafakardan");
+const nav = document.getElementById("nav");
+bars.addEventListener("click",()=>{
+    if(listbook.classList.contains("hidden")){
+        listbook.classList.remove("hidden");
+        listbook.classList.add("flex");
+        nav.classList.remove("w-full");
+        nav.classList.add("w-[85%]");
+    
+ }
+ 
+        else{
+            listbook.classList.remove("flex");
+            listbook.classList.add("hidden");
+            nav.classList.remove("w-[85%]");
+            nav.classList.add("w-full");
+             izafakardan.classList.remove("flex")
+        izafakardan.classList.add("hidden")
+        }
+});
+showform.addEventListener("click",()=>{
+    if(izafakardan.classList.contains("hidden")){
+    izafakardan.classList.remove("hidden");
+    izafakardan.classList.add("flex");
+    }
+
+    else{
+        izafakardan.classList.remove("flex")
+        izafakardan.classList.add("hidden")
+    }
+    
+})
