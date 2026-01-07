9d552bf28b1915ab4e40a3f78f4516e86972a0e4
shikebazahidi999 <shikebazahedi41@gmail.com>
2026-01-07 18:57:46 +0430

    changes

--- tailwind-projacts/bookProgram.js
+++ tailwind-projacts/bookProgram.js
@@ -81,7 +81,7 @@ bookinfo.forEach(book=>{
     newadded.appendChild(maindiv);
 maindiv.classList.add("divstyle")
     const divinfo = document.createElement("div");
-    divinfo
+    divinfo.classList
     maindiv.appendChild(divinfo);
    const h1 = document.createElement("h1");
    h1.innerHTML = "اسم کتاب:"+Allinfo.nameb;
