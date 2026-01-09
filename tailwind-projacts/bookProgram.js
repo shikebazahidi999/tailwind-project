f103854aefd187d141bb134463a31f87b42d0d84
shikebazahidi999 <shikebazahedi41@gmail.com>
2026-01-09 19:03:19 +0430

    chnages

--- tailwind-projacts/bookProgram.js
+++ tailwind-projacts/bookProgram.js
@@ -145,7 +145,7 @@ maindiv.classList.add("divstyle")
 })
     })
     
-// one at
+// one attr
  document.getElementById("bookname").value = "";
  document.getElementById("booktype").value = "";
  document.getElementById("bookimg").value = "";
