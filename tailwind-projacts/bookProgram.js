5c7a19417c271655dd652e07576a52903bcaa41a
shikebazahidi999 <shikebazahedi41@gmail.com>
2026-01-10 17:05:09 +0430

    changes

--- tailwind-projacts/bookProgram.js
+++ tailwind-projacts/bookProgram.js
@@ -103,7 +103,7 @@ maindiv.classList.add("divstyle")
     deletbtn.innerText = "Delet";
     deletbtn.classList.add("btn");
     maindiv.appendChild(deletbtn);
-    // remove butt
+    // remove buttu
     deletbtn.addEventListener("click",()=>{
         bookinfo.splice(index,1);
 
