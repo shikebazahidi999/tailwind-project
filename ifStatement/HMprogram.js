0b3da704f0ff88a4f896cb2e5dcdce16bc47747d
shikebazahidi999 <shikebazahedi41@gmail.com>
2026-01-14 19:22:59 +0430

    changes

--- ifStatement/HMprogram.js
+++ ifStatement/HMprogram.js
@@ -159,7 +159,7 @@ else{
 // }
 // else if (opretor==="*"){
 //     let resuilt1 = firstnum*secoundnum
-//     console.log(firstnum+"*"+secoundnum+"="+resuilt1)
+//     console.log(firstnum+"*"+secoundnum+"="+resuilt1);
 // }
 // else if (opretor==="/"){
 //     let resuilt1 = firstnum/secoundnum
