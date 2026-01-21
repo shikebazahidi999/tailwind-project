b74988714c83b4ce2c6cd0d99b1b2068903a5978
shikebazahidi999 <shikebazahedi41@gmail.com>
2026-01-21 18:33:01 +0430

    changes

--- ifStatement/HMprogram.js
+++ ifStatement/HMprogram.js
@@ -765,7 +765,7 @@ let password = prompt('enter your password to sign in');
 let r = 0;
 let numbers = [1,2,3,4,5,6,7,8,9,0];
 let srring = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','r','s','t','u','v','w','x','y','z'];
-let string = []
+let string = [];
 let w = 0;
 let q = 0;
 do{
