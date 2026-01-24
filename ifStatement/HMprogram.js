6f984c8dbc4e4b645c53a25c4bd3ffdc1116e3e6
shikebazahidi999 <shikebazahedi41@gmail.com>
2026-01-24 20:07:13 +0430

    changes

--- ifStatement/HMprogram.js
+++ ifStatement/HMprogram.js
@@ -979,7 +979,7 @@ let problem6 = ''
 // thirteenth one 13
 function perfectnum(number){
    if(number%2===0&&number%3===0){
-    return number
+    return number + "this is a perfect number"
    } 
 }
 
