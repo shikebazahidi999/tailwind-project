df948dc9f7997625e50a25f15201c4eae9cfebb1
shikebazahidi999 <shikebazahedi41@gmail.com>
2026-01-31 09:27:19 +0430

    changes

--- ifStatement/HMprogram.js
+++ ifStatement/HMprogram.js
@@ -985,13 +985,14 @@ let promlep3 = ''
 // }else{
 //     console.log(`your name ${name} is not valid`);
 // };
-// split1 && includes2 && tolowercase2 && lenght2
-let litters = prompt("enter many names put (,) between names")
+// split1 && includes2 && tolowercase2 && lenght2 && trim
+let litters = prompt("enter many names put (,) between names").toLowerCase.trim();
 let names = ["ali","mohammad","mahdi","sohaila","amir","naqi"];
 for(let i = 0; i<names.length;i++){
     if(litters.includes(names[i])){
-        console.log(litters.split(","))
+        console.log(litters.split(","));
         break;   
     }
-}
+};
+// slice&& 
 
