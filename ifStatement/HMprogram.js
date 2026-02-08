b0b392fe895290abd7900dc692a70d7c85509127
shikebazahidi999 <shikebazahedi41@gmail.com>
2026-02-08 20:53:28 +0430

    changes

--- ifStatement/HMprogram.js
+++ ifStatement/HMprogram.js
@@ -1547,6 +1547,6 @@ console.log(resuilt);
 let numbers = [90,80,34,56,1,245];
 let sortednums = numbers.sort((smallnum,bignum)=> smallnum - bignum);
 console.log(sortednums);
-let strings = ["zahra","alimohammad","wahida","morteza"];
+let strings = ["zahra","alimohammad","wahida","morteza","bahara"];
 let sortedstring = strings.sort();
-console
+console.log(sortedstring)
