5e24c4b32b7031a8648a6e5fa9af5d8d3c89b8a8
shikebazahidi999 <shikebazahedi41@gmail.com>
2026-02-11 20:12:09 +0430

    changes

--- ifStatement/HMprogram.js
+++ ifStatement/HMprogram.js
@@ -1630,7 +1630,7 @@ console.log(resuiltofinfo);
 // object values
 let valueofaliinfo = Object.values(aliInfo);
 console.log(valueofaliinfo);
-// object entries
+// object entries;
 let entriesinfo = Object.entries(aliInfo);
 console.log(entriesinfo);
 // from entries;
