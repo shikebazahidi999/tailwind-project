28429930aa5e1ca7122791b00c937895d14804e8
shikebazahidi999 <shikebazahedi41@gmail.com>
2026-02-18 19:59:36 +0430

    changes

--- ifStatement/light.js
+++ ifStatement/light.js
@@ -3,6 +3,6 @@ let light = document.getElementById("light");
 button.addEventListener("click",()=>{
     light.classList.toggle("light");
     if(button.textContent==="turn on the light"){
-        
+        button.textContent= "turn of the light"
     }
 })
