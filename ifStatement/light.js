25c6cc8580fc5b796001cb489f75da848c6005b2
shikebazahidi999 <shikebazahedi41@gmail.com>
2026-02-19 19:25:04 +0430

    changes

--- ifStatement/light.js
+++ ifStatement/light.js
@@ -11,7 +11,7 @@ button.addEventListener("click",()=>{
         light.classList.add("light");
     }
 });
-// 
+// b
 button2.addEventListener("click",()=>{
             button.classList.remove("hidden");
         button.classList.add("block");
