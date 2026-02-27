ea3b752acf297a8e05e0e707e605e034437721d1
shikebazahidi999 <shikebazahedi41@gmail.com>
2026-02-27 22:11:35 +0430

    changes

--- tailwind-projacts/todo.js
+++ tailwind-projacts/todo.js
@@ -7,10 +7,12 @@ let showtodo = document.getElementById("showtodo");
 showtodo.addEventListener("click",()=>{
 if(addDiv.classList.contains("flex")){
     addDiv.classList.remove("flex");
-    addDiv.classList.add("hidden");   
+    addDiv.classList.add("hidden"); 
+    showtodo.textContent = "add Todo"  
 }
 else{
     addDiv.classList.remove("hidden");
-    addDiv.classList.add("flex")
+    addDiv.classList.add("flex");
+    showtodo.textContent = "Hide Todo"
 }
 })
