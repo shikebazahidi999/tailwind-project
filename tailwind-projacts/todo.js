629d0b45c1e5a293b4fe6d1581e874a4515b4d47
shikebazahidi999 <shikebazahedi41@gmail.com>
2026-02-27 22:03:35 +0430

    changes

--- tailwind-projacts/todo.js
+++ tailwind-projacts/todo.js
@@ -2,3 +2,8 @@ let addDiv = document.getElementById("addDiv");
 let input = document.getElementById("input");
 let importance = document.getElementById("importance");
 let addbtn = document.getElementById("addbtn");
+let showtodo = document.getElementById("showtodo");
+
+showtodo.addEventListener("click",()=>{
+
+})
