e2abc1b2f40ecd2e921b49ccd7f3491990d3be97
shikebazahidi999 <shikebazahedi41@gmail.com>
2026-02-27 22:00:35 +0430

    changes

---
+++ tailwind-projacts/todo.js
@@ -0,0 +1,4 @@
+let addDiv = document.getElementById("addDiv");
+let input = document.getElementById("input");
+let importance = document.getElementById("importance");
+let addbtn = document.getElementById("addbtn")
