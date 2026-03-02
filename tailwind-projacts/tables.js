ede6ffc8d1c40efda3887957338a757442b5923c
shikebazahidi999 <shikebazahedi41@gmail.com>
2026-03-02 16:38:27 +0430

    changes

--- tailwind-projacts/tables.js
+++ tailwind-projacts/tables.js
@@ -1,18 +1,18 @@
-class studentsInfo {
-    constructor(name,fathername,lastname,age,gender,country){
-        this.name =name;
-        this.lastname =lastname;
-        this.fathername =fathername;
-        this.age =age;
-        this.gender =gender;
-        this.country =country;
-    }
-};
-let names = prompt("enter your name");
-let lastnames = prompt("enter your lastname");
-let fathernames = prompt("enter your father's name");
-let ages = prompt("enter your age");
-let genders = prompt("enter your gender");
-let countrys = prompt("enetr your country");
-let studentsInfos = new studentsInfo(names,lastnames,fathernames,ages,genders,countrys);
-console.log(studentsInfos);
+// class studentsInfo {
+//     constructor(name,fathername,lastname,age,gender,country){
+//         this.name =name;
+//         this.lastname =lastname;
+//         this.fathername =fathername;
+//         this.age =age;
+//         this.gender =gender;
+//         this.country =country;
+//     }
+// };
+// let names = prompt("enter your name");
+// let lastnames = prompt("enter your lastname");
+// let fathernames = prompt("enter your father's name");
+// let ages = prompt("enter your age");
+// let genders = prompt("enter your gender");
+// let countrys = prompt("enetr your country");
+// let studentsInfos = new studentsInfo(names,lastnames,fathernames,ages,genders,countrys);
+// console.log(studentsInfos);
