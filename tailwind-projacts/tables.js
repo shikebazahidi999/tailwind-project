e69197da97d8b116bb710e13e65f185bf1810ce0
shikebazahidi999 <shikebazahedi41@gmail.com>
2026-03-03 19:12:29 +0430

    changes

--- tailwind-projacts/tables.js
+++ tailwind-projacts/tables.js
@@ -2,7 +2,7 @@ class studentsInfo {
     name;
     lastname;
     fathername;
-    age
+    age;
     constructor(name,fathername,lastname,age,gender,country){
         this.name =name;
         this.lastname =lastname;
