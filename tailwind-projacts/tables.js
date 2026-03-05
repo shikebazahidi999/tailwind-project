bd9b2e8bad0ca424f56a8efd6a0db7fdeb205f28
shikebazahidi999 <shikebazahedi41@gmail.com>
2026-03-05 09:59:34 +0430

    changes

--- tailwind-projacts/tables.js
+++ tailwind-projacts/tables.js
@@ -1,24 +1,24 @@
-class studentsInfo {
-    name;
-    lastname;
-    fathername;
-    age;
-    gender;
-    country;
-    constructor(name,fathername,lastname,age,gender,country){
-        this.name =name;
-        this.lastname =lastname;
-        this.fathername =fathername;
-        this.age =age;
-        this.gender =gender;
-        this.country =country;
-    }
-};
-let names = prompt("enter your name");
-let lastnames = prompt("enter your lastname");
-let fathernames = prompt("enter your father's name");
-let ages = prompt("enter your age");
-let genders = prompt("enter your gender");
-let countrys = prompt("enetr your country");
-let studentsInfos = new studentsInfo(names,lastnames,fathernames,ages,genders,countrys);
-console.log(studentsInfos);
+// class studentsInfo {
+//     name;
+//     lastname;
+//     fathername;
+//     age;
+//     gender;
+//     country;
+//     constructor(name,fathername,lastname,age,gender,country){
+//         this.name =name;
+//         this.lastname =lastname;
+//         this.fathername =fathername;
+//         this.age =age;
+//         this.gender =gender;
+//         this.country =country;
+//     }
+// };
+// let names = prompt("enter your name");
+// let lastnames = prompt("enter your lastname");
+// let fathernames = prompt("enter your father's name");
+// let ages = prompt("enter your age");
+// let genders = prompt("enter your gender");
+// let countrys = prompt("enetr your country");
+// let studentsInfos = new studentsInfo(names,lastnames,fathernames,ages,genders,countrys);
+// console.log(studentsInfos);
